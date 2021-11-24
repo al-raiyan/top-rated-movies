ed13dd2a12f0dffd63783a965c385660f092702e
al-raiyan <alraiyan5@gmail.com>
2021-11-25 01:09:11 +0600

    2nd requarment

---
+++ src/Componets/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header bg-warning">
+      <h1>Top Rated Movies Of All Time</h1>
+      <p>
+        Here you can see the top rated movies of all time.List by the rating of
+        each movies you can see in this page.
+      </p>
+      <h5>The Budget Of the Movies are 1 Billion</h5>
+    </div>
+  );
+};
+
+export default Header;
